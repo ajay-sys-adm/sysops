f4a797b5c0a8f0f6662e346d7b2dc33503c19cf4
jay <jay@gmail.com>
2019-11-16 08:10:35 -0800

    gone

---
+++ java.java
@@ -0,0 +1,8 @@
+class Hello{
+       public static void main(String[] args){
+          for(int i=1;1<=10;i++){
+              system.out.println("Hello World......."+i);
+ 
+             }
+       }
+  }
